151da4659b95a52b01f84ab3ed5ed40c03dc04d5
Amit Melamed <amitpay1234@gmail.com>
2023-04-20 14:34:42 +0300

    Death added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,8 +8,10 @@ public class PlayerController : MonoBehaviour
     Animator animator;
     [SerializeField] float moveSpeed =3f;
     [SerializeField] float jumpForce = 3f;
+    [SerializeField] GameObject fireEffect;
     bool isInAir =false;
     float moveHorizontal = 0;
+    bool death = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,7 +28,7 @@ public class PlayerController : MonoBehaviour
     }
     void FixedUpdate()
     {
-        if (moveHorizontal > 0.1f || moveHorizontal < -0.1f)
+        if ((moveHorizontal > 0.1f || moveHorizontal < -0.1f ) && !death)
         {
             rb.AddForce(new Vector2(moveHorizontal * moveSpeed, 0f), ForceMode2D.Impulse);
             animator.SetBool("Walking", true);
@@ -43,7 +45,7 @@ public class PlayerController : MonoBehaviour
         {
             animator.SetBool("Walking", false);
         }
-        if (!isInAir && Input.GetKey(KeyCode.Space) && rb.velocity.y<1f)
+        if (!isInAir && Input.GetKey(KeyCode.Space) && rb.velocity.y<1f && !death)
         {
             rb.AddForce(new Vector2(0f , jumpForce), ForceMode2D.Impulse);
         }
@@ -55,6 +57,10 @@ public class PlayerController : MonoBehaviour
             isInAir = false;
             animator.SetBool("InAir", false);
         }
+        if (collision.gameObject.tag == "DeadGround") 
+        {
+            KillPlayer();
+        }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
@@ -63,6 +69,13 @@ public class PlayerController : MonoBehaviour
             isInAir = true ;
             animator.SetBool("InAir", true);
         }
+    }
+
+    void KillPlayer() 
+    {
+        death = true;
+        animator.SetBool("Death", true);
+        Instantiate(fireEffect, transform.position - new Vector3(0,1,0), Quaternion.identity);
     }
 
 
